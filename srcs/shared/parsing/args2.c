6fe47912aa012525ea9a49fce98ac3ad92010f23
clbrunet <brunet.clement5@gmail.com>
2021-04-20 18:48:01 +0200

    fix: return error with a really really long number

--- srcs/shared/parsing/args2.c
+++ srcs/shared/parsing/args2.c
@@ -6,7 +6,7 @@
 /*   By: clbrunet <clbrunet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 07:09:41 by clbrunet          #+#    #+#             */
-/*   Updated: 2021/04/19 07:15:02 by clbrunet         ###   ########.fr       */
+/*   Updated: 2021/04/20 18:47:16 by clbrunet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,18 @@
 
 long int	get_longv(const char **arg, char sign)
 {
-	long int	longv;
+	long int		longv;
+	unsigned int	count;
 
 	longv = 0;
+	count = 0;
 	while (ft_isdigit(**arg))
 	{
 		longv = longv * 10 + **arg - '0';
 		(*arg)++;
+		count++;
+		if (count > 10)
+			return ((long int)INT_MAX + 1);
 	}
 	return (longv * sign);
 }
